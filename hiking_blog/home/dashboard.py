b52316105d02c85edea0485e12fac2e77421f084
gruvis41 <dougheinan@gmail.com>
2023-03-02 12:40:33 -0700

    changed body text on home page.

--- hiking_blog/home/dashboard.py
+++ hiking_blog/home/dashboard.py
@@ -6,6 +6,14 @@ from hiking_blog.db import db
 # This import only exists for development purposes
 from hiking_blog.dev_db_autofill import create_database
 
+WELCOME_TEXT = "Tamarack Treks is an online resource and community that aims to help you explore Western Montana in" \
+               " the best way possible. Discover new local trails or explore some hidden gems in a new mountain range." \
+               " Search for a new campsite - free, paid, car-camping or backpacking - and read campsite reviews." \
+               " Explore gear reviews focused on gear tested during all four seasons in Western Montana. Western" \
+               " Montana is situated in the northern Rocky Mountains where a number of unique ranges, rivers, and" \
+               " habitats create a beautiful, wild outdoor space. The hiking, camping and outdoor recreational" \
+               " opportunities are limitless. We will help you explore it all."
+
 home_bp = Blueprint(
     "home_bp",
     __name__,
@@ -32,8 +40,16 @@ def home():
     # End of development only code.
     recent_trails = db.session.query(Trails).order_by(Trails.date_time_added)[:-4:-1]
     recent_gear = db.session.query(Gear).order_by(Gear.date_time_added)[:-4:-1]
-    return render_template("dashboard.html", all_trails=saved_trails, all_gear=saved_gear, recent_trails=recent_trails,
-                           recent_gear=recent_gear, logged_in=current_user.is_authenticated, user=current_user)
+    return render_template(
+        "dashboard.html",
+        all_trails=saved_trails,
+        all_gear=saved_gear,
+        recent_trails=recent_trails,
+        recent_gear=recent_gear,
+        logged_in=current_user.is_authenticated,
+        user=current_user,
+        body_text=WELCOME_TEXT
+    )
 
 
 @home_bp.route("/home/static/dev_pics/<file_name>")
