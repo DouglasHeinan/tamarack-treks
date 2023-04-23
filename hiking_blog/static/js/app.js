ae12713b8f8f32793e73205c9c8e14332a13c551
gruvis41 <dougheinan@gmail.com>
2023-04-23 17:27:14 -0600

    Navbar is now closed when returning to page using back button (fixed using bootstrap)

--- hiking_blog/static/js/app.js
+++ hiking_blog/static/js/app.js
@@ -1,6 +1,6 @@
-const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('navbarSupportedContent')
-const bsCollapse = new bootstrap.Collapse(menuToggle)
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => { bsCollapse.toggle() })
-})
+//const navLinks = document.querySelectorAll('.nav-item')
+//const menuToggle = document.getElementById('navbarSupportedContent')
+//const bsCollapse = new bootstrap.Collapse(menuToggle)
+//navLinks.forEach((l) => {
+//    l.addEventListener('click', () => { bsCollapse.toggle() })
+//})
